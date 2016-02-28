96ff7d1a6cb27aed5c08117f0b700d58d088e711
benzhu <benzhu@welinktech.com.cn>
2016-02-28 10:45:42 +0800

    异常信息编码更改

--- throwException/exception.php
+++ throwException/exception.php
@@ -1,15 +1,15 @@
 <?php
 /*
- * �׳��쳣
- * Exception���м������������뷽�������а����ˣ�
- *	message �쳣��Ϣ����
- *	code �쳣����
- *	file �׳��쳣���ļ���
- *	line �׳��쳣�ڸ��ļ�������
- *  ���õķ�����
- *  getTrace() ��ȡ�쳣׷����Ϣ
- *	getTraceAsString() ��ȡ�쳣׷����Ϣ���ַ���
- *	getMessage() ��ȡ������Ϣ
+ * 抛出异常
+ * Exception具有几个基本属性与方法，其中包括了：
+ *	message 异常消息内容
+ *	code 异常代码
+ *	file 抛出异常的文件名
+ *	line 抛出异常在该文件的行数
+ *  常用的方法：
+ *  getTrace() 获取异常追踪信息
+ *	getTraceAsString() 获取异常追踪信息的字符串
+ *	getMessage() 获取出错信息
  *  
  */
  try {
@@ -17,18 +17,18 @@
  } catch (Exception $e) {
  	echo $e->getMessage();
  }
-//����һ�����׳��쳣����
+//创建一个可抛出异常函数
 function throwException($number){
 	if($number>1){
-		throw  new Exception('�쳣��Ϣ��ʾ�����ֱ���С��1<br/>');
+		throw  new Exception('异常信息提示：数字必须小于1<br/>');
 	}
 }	
 /*
- * ͨ���̳л���Exception���Զ����Լ����쳣��
+ * 通过继承基类Exception，自定义自己的异常类
  */
 	class myException extends Exception {
 		function getmydefineException() {
-			return '�Զ��������Ϣ';
+			return '自定义错误信息';
 		}
 	}
 	try {
