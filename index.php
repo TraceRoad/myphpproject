4e4abb027b1e8d914a9704f5a69cd16667c082b7
benzhu <benzhu@welinktech.com.cn>
2016-03-05 21:25:31 +0800

    更改测试

--- index.php
+++ index.php
@@ -1,10 +1,2 @@
 <?php
-class Test{
-    public  function test(){
-        $a=1;
-        echo $a;
-    }
-    public function  connectmysql(){
-       $conn = mysql_connect("localhost","root","root");
-    }
-}
+echo "Success";
